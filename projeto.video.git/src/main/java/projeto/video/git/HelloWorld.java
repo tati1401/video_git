57f5ae0d1284d23d49b41a6224f08086379167de
tati1401 <tatiane.maldonado35@gmail.com>
2023-08-23 17:39:44 -0300

    inclusão da mensagem git hub

--- projeto.video.git/src/main/java/projeto/video/git/HelloWorld.java
+++ projeto.video.git/src/main/java/projeto/video/git/HelloWorld.java
@@ -4,10 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		System.out.println("Hello World");
 
+		System.out.println("Hello World");
+		System.out.println("Git Hub");
 	}
 
 }
